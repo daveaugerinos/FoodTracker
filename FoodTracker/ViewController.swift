415864622cb7b35ae05b71bf28461963afb7d8ae
daveaugerinos <dave.augerinos@gmail.com>
2017-03-13 12:19:03 -0700

    Build basic UI and connect to code

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Dave Augerinos on 2017-03-13.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self;
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        
+        mealNameLabel.text = "Default Text"
+    }
+
+    
+
+}
+
